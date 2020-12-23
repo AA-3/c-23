10782f5090a6b6249c1a46384831d18cc52829d8
AA-3 <74598787+AA-3@users.noreply.github.com>
2020-12-23 15:03:52 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,18 @@
+class Box{
+    constructor(x,y,width,height){
+        var options={
+            resitution:1,
+            friction:1,
+            density:0.3
+        }
+        this.body=Bodies.rectangle(x,y,width,height,options);
+        this.width=width ;
+        this.height=height;
+        World.add(world,this.body);
+    }
+    display(){
+        var pos=this.body.position;
+        rectMode(CENTER);
+    rect(pos.x,pos.y,this.width,this.height);
+    }
+}
